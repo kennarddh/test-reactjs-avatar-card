8cbfd7232fbd12f2017063c797955177e91ec725
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-09-01 19:38:27 +0700

    feat: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-// import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Card from '@/Components/Card/Card'
+
+import { Cards } from '@/Styles'
 
 const App = () => {
-	return null
+	const [Users, SetUsers] = useState()
+
+	useEffect(() => {
+		const controller = new AbortController()
+
+		const signal = controller.signal
+
+		fetch('https://jsonplaceholder.typicode.com/users', { signal })
+			.then(response => response.json())
+			.then(data => SetUsers(data))
+			.catch(console.log)
+
+		return () => controller.abort()
+	}, [])
+
+	return (
+		<Cards>
+			{Users.map(user => (
+				<Card user={user} key={user.id} />
+			))}
+		</Cards>
+	)
 }
 
 export default App
---
+++ src/Styles.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Cards = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	flex-wrap: wrap;
+
+	width: 100%;
+
+	margin: 10px 20px;
+`
