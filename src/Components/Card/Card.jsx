d601db4dd0627b6e2ad26a83d79520bf02be4823
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-09-01 20:13:07 +0700

    feat: Card style

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 
-import { Container } from './Styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faHeart,
+	faPenToSquare,
+	faEnvelope,
+} from '@fortawesome/free-regular-svg-icons'
+import { faTrash, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+import {
+	Container,
+	Image,
+	Name,
+	BodyText,
+	Body,
+	ActionButton,
+	Actions,
+	ActionDivider,
+	BodyIcon,
+} from './Styles'
 
 const Card = ({ user }) => {
-	return <Container>{user.id}</Container>
+	return (
+		<Container>
+			<Image
+				src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+			/>
+			<Body>
+				<Name>{user.username}</Name>
+				<BodyText>
+					<BodyIcon>
+						<FontAwesomeIcon icon={faEnvelope} />
+					</BodyIcon>
+					{user.email}
+				</BodyText>
+				<BodyText>
+					<BodyIcon>
+						<FontAwesomeIcon icon={faPhone} />
+					</BodyIcon>
+					{user.phone}
+				</BodyText>
+				<BodyText>
+					<BodyIcon>
+						<FontAwesomeIcon icon={faGlobe} />
+					</BodyIcon>
+					{user.website}
+				</BodyText>
+			</Body>
+			<Actions>
+				<ActionButton>
+					<FontAwesomeIcon icon={faHeart} />
+				</ActionButton>
+				<ActionDivider />
+				<ActionButton>
+					<FontAwesomeIcon icon={faPenToSquare} />
+				</ActionButton>
+				<ActionDivider />
+				<ActionButton>
+					<FontAwesomeIcon icon={faTrash} />
+				</ActionButton>
+			</Actions>
+		</Container>
+	)
 }
 
 export default Card
--- src/Components/Card/Styles.jsx
+++ src/Components/Card/Styles.jsx
@@ -2,9 +2,72 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 	width: calc(25% - 15px);
-	height: 75%;
+	height: 400px;
 
-	border: 1px solid black;
+	border: 1px solid #8b8b8b;
 
 	border-radius: 5px;
+`
+
+export const Image = styled.img`
+	height: 50%;
+	width: 100%;
+
+	background-color: #dadada;
+`
+
+export const Body = styled.div`
+	height: 35%;
+	width: 100%;
+
+	padding: 10px 20px;
+
+	display: flex;
+	align-items: flex-start;
+	gap: 10px;
+
+	flex-direction: column;
+`
+
+export const Name = styled.h3``
+
+export const BodyText = styled.p`
+	color: #7e7e7e;
+`
+
+export const BodyIcon = styled.span`
+	font-size: 1.2rem;
+	margin-right: 10px;
+`
+
+export const Actions = styled.div`
+	height: 14%;
+	width: 100%;
+
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+
+	background-color: #dadada;
+
+	border-radius: 0 0 5px 5px;
+`
+
+export const ActionButton = styled.button`
+	background-color: transparent;
+
+	border: none;
+
+	font-size: 1.5rem;
+`
+
+export const ActionDivider = styled.div`
+	background-color: #bebebe;
+
+	border-radius: 5px;
+
+	width: 5px;
+	height: 70%;
+
+	color: rgba(0, 0, 0, 0);
 `
