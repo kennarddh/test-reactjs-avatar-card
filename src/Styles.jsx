5df330e66bfdc7c79a03210c2c580e5821f81b37
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-09-01 21:47:45 +0700

    refactor: Resposive without media

--- src/Styles.jsx
+++ src/Styles.jsx
@@ -1,27 +1,37 @@
 import styled from 'styled-components'
 
 export const Cards = styled.div`
-	--max: 4;
+	/**
+   * User input values.
+   */
+	--grid-layout-gap: 20px;
+	--grid-column-count: 4;
+	--grid-item--min-width: 250px;
+
+	/**
+   * Calculated values.
+   */
+	--gap-count: calc(var(--grid-column-count) - 1);
+	--total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
+	--grid-item--max-width: calc(
+		(100% - var(--total-gap-width)) / var(--grid-column-count)
+	);
 
 	display: grid;
-	grid-template-columns: repeat(var(--max), calc((100% / var(--max)) - 15px));
-	gap: 20px;
+
+	grid-template-columns: repeat(
+		auto-fill,
+		minmax(
+			max(var(--grid-item--min-width), var(--grid-item--max-width)),
+			1fr
+		)
+	);
+
+	grid-gap: var(--grid-layout-gap);
 
 	max-width: 100%;
 
 	margin: 30px 20px;
 
 	min-height: 100%;
-
-	@media screen and (max-width: 1200px) {
-		--max: 3;
-	}
-
-	@media screen and (max-width: 900px) {
-		--max: 2;
-	}
-
-	@media screen and (max-width: 650px) {
-		--max: 1;
-	}
 `
